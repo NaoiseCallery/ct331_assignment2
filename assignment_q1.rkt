2a986074467ecd692def31d3576a693d23fa36f3
Naoise Callery <naoisecallery@gmail.com>
2017-10-26 17:42:46 +0100

    Question 1
    Question 1 A complete

--- assignment_q1.rkt
+++ assignment_q1.rkt
@@ -1,1 +1,6 @@
 #lang racket
+(cons 2 1)
+(cons 3 (cons 2 (cons 1 '())))
+(cons "DrRacket"(cons 4 (cons 3(cons 2(cons 1 '())))))
+(list "DrRacket" 4 (list 3 2 1))
+(append '(DrRacket) '(4) '(( 3 2 1)))
